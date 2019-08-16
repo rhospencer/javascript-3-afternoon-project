ff20cc5a80fef5539686ce0364c71840750a4b81
Spencer Rhoton <rhotonspencer@gmail.com>
2019-08-16 16:22:24 -0600

    finished spread

--- spread.js
+++ spread.js
@@ -17,7 +17,7 @@ let product = {
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg'
 }
 
-// code here
+let productCopy = {...product}
 
 
 
@@ -28,7 +28,7 @@ let product = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = {...colors}
 
 
 
@@ -42,8 +42,9 @@ let numbers = [4, 6, 10];
 function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
+result = addNums(...numbers)
 
-// code here
+
 
 
 ////////// PROBLEM 4 //////////
@@ -53,4 +54,4 @@ function addNums(num1, num2, num3) {
 let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
-// code here
+let combinedArrays = [...arr1, ...arr2]

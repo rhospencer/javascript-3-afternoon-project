c83f9be9b676e8d0771bbd1e3039a7a94313a6b9
Spencer Rhoton <rhotonspencer@gmail.com>
2019-08-16 17:20:23 -0600

    finished higher-order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,13 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index, wholeArray) {
+  if (element % 2 === 0) {
+    return true
+  } else {
+    return false 
+  }
+})
 
 
 
@@ -44,7 +50,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, wholeArray) {
+  return element * 1.07
+})
 
 
 
@@ -63,7 +71,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(runningTotal, curElement, curIndex, wholeArray) {
+  return runningTotal + curElement
+})
 
 
 
@@ -89,7 +99,13 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(el, i, arr) {
+  if (el.CP > 200) {
+    return true
+  } else {
+    return false
+  }
+})
 
 
 
@@ -106,7 +122,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(el, i, arr) {
+  total = (el.tax * el.price) + el.price
+  return total
+})
 
 
 
@@ -126,6 +145,10 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce(function(acc, el, i, arr) {
+  if (el.owner === 'Bob') {
+    return acc + el.price
+  } return acc
+}, 0)
 
 
